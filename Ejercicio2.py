40ac2b99c0ede22e711dfc355621b7859f3c9181
Gustavo <perezgustavo2103@gmail.com>
2022-07-19 15:43:08 -0500

    Subido por Gustavo Rafael Perez Garcia

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,13 +1,17 @@
 print("GENERADOR DE LISTAS")
+
 n_list=int(input("¿Cuantas listas quiere escribir? ", ))
 
+listas_palabras=[]
 for i in range(n_list):
-    i=i+1
-    n_pala=int(input("Digite el número de palabras que posee la lista "+format(i)+": ", ))
-    palabras=[]
     
-    for j in range(n_pala):
-        
-        palabras[j]=str(input())
+    n_pala=int(input("Digite el número de palabras que posee la lista "+format(i+1)+": ", ))
+    palabras=[]
 
-print(palabras)
+    for j in range(n_pala):
+        palabras.append(input("Escriba la palabra "+format(j+1)+": ", ))
+    
+    listas_palabras.append(palabras)
+    
+for i in range(n_list):
+    print("La lista", i+1,"es :",listas_palabras[i])
